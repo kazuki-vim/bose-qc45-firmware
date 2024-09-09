e5325b6a94bb9eca4661d92f3f3ba0e09c0c64d6
valievkarim <valievkarim@gmail.com>
2024-09-09 17:32:02 +0400

    Add more firmware versions. Support MacOS. Do not wait for download completion, instead overwrite detected file in endless loop. Change print to logging

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import logging
 import os
 import time
 import shutil
+import sys
+import tempfile
+
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from datetime import datetime
 
-# Define the directory to monitor
-monitor_directory = os.path.join(os.getenv('LOCALAPPDATA'), 'Temp')
-pattern = "Bose Updater"
-print("directory: ", monitor_directory)
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(module)s:%(lineno)d - %(name)s - %(levelname)s - %(message)s")
+logging.getLogger("fsevents").setLevel(logging.WARNING)
 
 # Function to check if a file with the given pattern is created
 def check_for_file_creation():
@@ -21,29 +23,34 @@ def check_for_file_creation():
 def is_file_complete(filepath):
     # Check if the file size remains constant over a short interval
     initial_size = os.path.getsize(filepath)
+    logger.info("initial_size %s", initial_size)
     time.sleep(0.5)  # Wait briefly
+    if initial_size == 0:
+        return False
     final_size = os.path.getsize(filepath)
+    logger.info("initial_size %s final_size %s", initial_size, final_size)
     return initial_size == final_size
 
 # Function to handle file creation event
 def handle_file_creation(filepath):
-    print(f"[datetime.now().strftime('%H:%M:%S')] Detected new file: {filepath}")
-
-    time_sleep = 3.5
-    print(f"Wait: {time_sleep}s")
-    time.sleep(time_sleep)
-
-    print(f"[datetime.now().strftime('%H:%M:%S')] File writing complete: {filepath}")
-
-    # Copy another file from download folder and rename it
-    download_folder = monitor_directory
-    file_to_copy = "bose.bin"
+    logger.info("Detected new file matching firmware download pattern: %s", filepath)
+    # while not is_file_complete(filepath):
+    #       logger.info("download is still not completed..")
+    #
+    # logger.info(f"Detected download completed: %s", filepath)
 
     try:
-        shutil.copy(os.path.join(download_folder, file_to_copy), os.path.join(monitor_directory, filepath))
-        print(f"[datetime.now().strftime('%H:%M:%S')] Copied {file_to_copy} to {filepath}")
-    except Exception as e:
-        print(f"Failed to copy {file_to_copy} to {filepath}: {e}")
+        while True:
+            try:
+                shutil.copy(firmware_path, os.path.join(monitor_directory, filepath))
+                logger.info("Copied %s to %s", firmware_path, filepath)
+            except Exception as e:
+                logger.exception("Failed to copy %s to %s", firmware_path, filepath)
+                raise
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        logger.info("CTRL-C pressed, exiting..")
+        sys.exit(0)
 
 # Define a custom event handler to monitor filesystem events
 class CustomFileSystemEventHandler(FileSystemEventHandler):
@@ -51,8 +58,25 @@ class CustomFileSystemEventHandler(FileSystemEventHandler):
         if event.is_directory:
             return
         filepath = event.src_path
+        logger.debug(f"detected created file {filepath}")
         if os.path.basename(filepath).startswith(pattern):
             handle_file_creation(filepath)
+
+if len(sys.argv) == 1:
+    firmware_path = "firmware/duran_encrypted_prod_1.0.3-ee3f55e.bin"
+elif len(sys.argv) == 2:
+    firmware_path = sys.argv[1]
+else:
+    print(f'usage: {sys.argv[0]} [firmware-bin-file-path]', file=sys.stderr)
+    sys.exit(1)
+
+assert os.path.exists(firmware_path)
+
+monitor_directory = os.path.realpath(tempfile.gettempdir())
+
+
+pattern = "Bose Updater"
+logger.info("monitoring directory: %s", monitor_directory)
 
 # Set up filesystem event monitoring
 event_handler = CustomFileSystemEventHandler()
@@ -64,6 +88,7 @@ try:
     while True:
         time.sleep(0.1)
 except KeyboardInterrupt:
-    observer.stop()
+    logger.info("CTRL-C pressed, exiting..")
+    sys.exit(0)
 
-observer.join()
+
